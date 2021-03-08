6e91c22ab3dd4fd7ddc80da4aa7ef13bb78ae9e2
Zlatan Radetinac <zlatan.rad@gmail.com>
2021-03-08 01:52:26 +0100

    admin

--- SQLMonitoring/SQLMonitoring/SQLMonitoring/Controllers/HomeController.cs
+++ SQLMonitoring/SQLMonitoring/SQLMonitoring/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SQLMonitoring.Model;
 using SQLMonitoring.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SQLMonitoring.DatabaseConnection;
+using Microsoft.Extensions.Configuration;
 
 namespace SQLMonitoring.Controllers
 {
@@ -13,9 +16,15 @@ namespace SQLMonitoring.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private DatabaseContext _db;
+
+        private IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, DatabaseContext db, IConfiguration configuration)
         {
             _logger = logger;
+            _db = db;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
@@ -26,6 +35,37 @@ namespace SQLMonitoring.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            ViewBag.ErrorMessage = "";
+            ViewBag.IsAdmin = false;
+
+            var adminUsername = _configuration.GetValue<string>("AdminUser:Username");
+            var adminPassword = _configuration.GetValue<string>("AdminUser:Password");
+
+            if (adminUsername == email && adminPassword == password)
+            {
+                var users = _db.Users.ToList<User>();
+                ViewBag.IsAdmin = true;
+                return View("../Admin/AdminHomepage", users);
+            }
+
+
+            var user = _db.Users.Where(user => user.Email == email && user.Password == password).FirstOrDefault();
+
+            if (user != null)
+            {
+                return View("Homepage", user);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Wrong username or password";
+                return View("Index");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
