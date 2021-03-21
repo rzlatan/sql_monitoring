0bac2f7f2ee3244a0043db3440e1891be42498c9
Zlatan Radetinac <zlatan.rad@gmail.com>
2021-03-21 04:05:30 +0100

    added  code editor

---
+++ SQLMonitoring/SQLMonitoring/SQLMonitoring/Controllers/QueryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SQLMonitoring.DatabaseConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLMonitoring.Controllers
+{
+    public class QueryController : Controller
+    {
+
+        public IConfiguration _configuraton;
+
+        public DatabaseContext _db;
+
+        public QueryController(DatabaseContext db, IConfiguration config)
+        {
+            _db = db;
+            _configuraton = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View("../Query/Edit");
+        }
+    }
+}
